Bugfix for spatial derivatives
Copying a dictionary does not create new numpy arrays. This caused
`spharical_derivatives` and `spatial_derivatives` to access the same
memory. The effect was that the calculation of a spatial derivative
changed the spharical derivative to the same, influencing subsequent
derivatives. Mathematically, we produced a situation where
d/dx G(x) D(x) = G'(x) D(x) + G(x) D(x) D'(x)
was used as the product rule.

This is no longer the case since new numpy arrays are created correctly.

levitate/models.py
@@ -472,7 +472,9 @@ class TransducerArray:
             spherical_derivatives['zzx'] = np.empty(num_trans, complex)
             spherical_derivatives['zzy'] = np.empty(num_trans, complex)
 
-        spatial_derivatives = spherical_derivatives.copy()
+        spatial_derivatives = {}
+        for key in spherical_derivatives.keys():
+            spatial_derivatives[key] = np.empty(num_trans, complex)
         for idx in range(num_trans):
             # Derivatives of the omnidirectional green's function
             difference = focus - self.transducer_positions[idx]