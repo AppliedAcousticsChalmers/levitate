990a6aaa68e8bd189d648b4e2fc9a4d398a18618
Carl Andersson <carl.andersson@chalmers.se>
2019-01-08 16:54:58 +0100

    Create hardware submodule
    Due to the number of c++ files that will be needed for the upcoming
    ultrahaptics python wrapping, all hardware related implementations
    are moved to a separate folder.

--- levitate/hardware/_dragonfly.py
+++ levitate/hardware/_dragonfly.py
@@ -1,18 +1,4 @@
-import os
 import numpy as np
-
-
-def data_to_cpp(complex_values, filename, normalize=True):
-    directory = os.path.dirname(filename)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    data = np.asarray(complex_values)
-    normalization = np.max(np.abs(data))
-    (data / normalization).conj().astype(np.complex64).tofile(filename)
-
-
-def data_from_cpp(file, num_transducers):
-    return np.fromfile(file, dtype=np.complex64).conj().reshape((-1, num_transducers))
 
 
 def dragonfly_grid():
