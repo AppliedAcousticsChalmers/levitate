8344a924bc950c5574485510ef1682de6a335c7b
Carl Andersson <carl.andersson@chalmers.se>
2019-02-15 10:04:02 +0100

    Bump version
    This is a usable state of the package. Be prepared that major changes
    on the way in.

--- levitate/__init__.py
+++ levitate/__init__.py
@@ -3,7 +3,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 __all__ = ['transducers', 'arrays', 'cost_functions', 'hardware', 'materials']
-__version__ = '0.4.0.dev1'
+__version__ = '0.4.0'
 
 spatial_derivative_order = ['', 'x', 'y', 'z', 'xx', 'yy', 'zz', 'xy', 'xz', 'yz', 'xxx', 'yyy', 'zzz', 'xxy', 'xxz', 'yyx', 'yyz', 'zzx', 'zzy']
 num_spatial_derivatives = [1, 4, 10, 19]
