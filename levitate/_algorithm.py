AppliedAcousticsChalmers/levitate

commit f3bee4a947ee81810979612e281bbaaeed0b2008

Refinements to algorithm class
Enables additions with 0. Useful to add algorithms in place in a loop.
Centralizes the radd and rmul methods.
Makes cost functions not addable with bound algorithms.

levitate/_algorithm.py
@@ -105,7 +105,7 @@ class Algorithm:
                                    calc_values=self.calc_values, calc_jacobians=self.calc_jacobians)
 
     def __rmul__(self, weight):
-        return self * weight
+        return self.__mul__(weight)
 
     def __matmul__(self, position):
         position = np.asarray(position)
@@ -115,10 +115,15 @@ class Algorithm:
                               calc_values=self.calc_values, calc_jacobians=self.calc_jacobians)
 
     def __add__(self, other):
+        if other == 0:
+            return self
         if type(self) == type(other):
             return AlgorithmPoint(self, other)
         else:
             return NotImplemented
+
+    def __radd__(self, other):
+        return self.__add__(other)
 
     def __sub__(self, vector):
         return VectorBase(algorithm=self, target_vector=vector)
@@ -156,7 +161,10 @@ class BoundAlgorithm(Algorithm):
             return self._cashed_spatial_structures
 
     def __add__(self, other):
+        if other == 0:
+            return self
         try:
+            other.weights  # If other has weights it's a cost function type object and should not be addable with self
             if np.allclose(self.position, other.position):
                 return super().__add__(other)
             else:
@@ -268,6 +276,8 @@ class VectorBase:
         return obj
 
     def __add__(self, other):
+        if other == 0:
+            return self
         if type(self) == type(other) or type(other) == type(self).__bases__[1]:
             return AlgorithmPoint(self, other)
         else:
@@ -322,6 +332,8 @@ class AlgorithmPoint(Algorithm):
         return [algorithm.calc_values(**{key: requirements[key] for key in algorithm.calc_values.requires}) for algorithm in self.algorithms]
 
     def __add__(self, other):
+        if other == 0:
+            return self
         if type(other) in type(self).__bases__:
             new = type(self)(*self.algorithms, other)
         elif type(other) == type(self):
@@ -329,9 +341,6 @@ class AlgorithmPoint(Algorithm):
         else:
             return NotImplemented
         return new
-
-    def __radd__(self, other):
-        return self + other
 
     def __iadd__(self, other):
         add_element = False
@@ -454,15 +463,14 @@ class AlgorithmCollection(BoundAlgorithmPoint):
         return values
 
     def __add__(self, other):
+        if other == 0:
+            return self
         try:
             other.position
         except AttributeError:
             return NotImplemented
         else:
             return AlgorithmCollection(*self.algorithms, other)
-
-    def __radd__(self, other):
-        return self + other
 
     def __iadd__(self, other):
         if type(other) == type(self):

commit 75227add1b5ef9dd3c3db9b19c5bec2ff05afd70

Fix broken algorithm classes
Bug introduced in f3bee4a947ee81810979612e281bbaaeed0b2008
The issue is line 167, now removed in this commit. The problem was never
adding cost functions and bound algorithms. The issue was adding
cost-function-type objects to algorithm collections, and vice versa.

levitate/_algorithm.py
@@ -164,7 +164,6 @@ class BoundAlgorithm(Algorithm):
         if other == 0:
             return self
         try:
-            other.weights  # If other has weights it's a cost function type object and should not be addable with self
             if np.allclose(self.position, other.position):
                 return super().__add__(other)
             else:
@@ -465,6 +464,12 @@ class AlgorithmCollection(BoundAlgorithmPoint):
     def __add__(self, other):
         if other == 0:
             return self
+        elif isinstance(self, CostFunctionCollection) and not isinstance(other, CostFunction):
+            # Make sure that we are not adding bound algorithms to cost function collections
+            return NotImplemented
+        elif isinstance(other, CostFunction) and not isinstance(self, CostFunctionCollection):
+            # Make sure that we are not adding cost functions to algorithm collections
+            return NotImplemented
         try:
             other.position
         except AttributeError:
@@ -477,6 +482,12 @@ class AlgorithmCollection(BoundAlgorithmPoint):
             for algorithm in other.algorithms:
                 self += algorithm
             return self
+        elif isinstance(self, CostFunctionCollection) and not isinstance(other, CostFunction):
+            # Make sure that we are not adding bound algorithms to cost function collections
+            return NotImplemented
+        elif isinstance(other, CostFunction) and not isinstance(self, CostFunctionCollection):
+            # Make sure that we are not adding cost functions to algorithm collections
+            return NotImplemented
         try:
             other_pos = other.position
         except AttributeError: