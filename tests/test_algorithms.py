921b44e151df43e52e0aafd4654fd81a3a2aa506
Carl Andersson <carl.andersson@chalmers.se>
2019-06-19 11:28:06 +0200

    Changes algorithm tests to be more flexible
    Some of the new algorithms have different requirements, so we will need
    a more flexible call in the test.
    Also merges the real and imaginary parts in the test fixture.

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -83,52 +83,50 @@ spat_ders = array.pressure_derivs(both_pos, orders=3)
 ind_ders = np.einsum('i, ji...->ji...', array.amplitudes * np.exp(1j * array.phases), spat_ders)
 sum_ders = np.sum(ind_ders, axis=1)
 
+requirements = dict(
+    pressure_derivs_summed=sum_ders,
+    pressure_derivs_individual=ind_ders,
+)
 
-@pytest.mark.parametrize("algorithm, value_at_pos_1, real_jacobian_at_pos_1, imag_jacobian_at_pos_1", [
+
+@pytest.mark.parametrize("algorithm, value_at_pos_1, jacobian_at_pos_1", [
     (levitate.algorithms.GorkovPotential,
         -6.19402404e-13,
-        [-6.08626619e-13, -6.30178190e-13],
-        [-1.21656276e-12, 1.21656276e-12]
+        [-6.08626619e-13 - 1.21656276e-12j, -6.30178190e-13 + 1.21656276e-12j],
      ),
     (levitate.algorithms.GorkovGradient,
         [2.30070037e-11, -1.62961537e-12, -2.44442306e-12],
-        [[2.30839871e-11, 2.29300203e-11], [-1.69118632e-12, -1.56804442e-12], [-2.53677948e-12, -2.35206663e-12]],
-        [[1.79047948e-11, -1.79047948e-11], [9.84604578e-13, -9.84604578e-13], [1.47690687e-12, -1.47690687e-12]]
+        [[2.30839871e-11 + 1.79047948e-11j, 2.29300203e-11 - 1.79047948e-11j], [-1.69118632e-12 + 9.84604578e-13j, -1.56804442e-12 - 9.84604578e-13j], [-2.53677948e-12 + 1.47690687e-12j, -2.35206663e-12 - 1.47690687e-12j]],
      ),
     (levitate.algorithms.GorkovLaplacian,
         [-3.98121194e-10, 8.74737783e-12, 2.98666962e-11],
-        [[-3.98912624e-10, -3.97329763e-10], [8.96724049e-12, 8.52751518e-12], [3.07462056e-11, 2.89871868e-11]],
-        [[3.33886801e-10, -3.33886801e-10], [1.94724287e-11, -1.94724287e-11], [3.76591861e-11, -3.76591861e-11]]
+        [[-3.98912624e-10 + 3.33886801e-10j, -3.97329763e-10 - 3.33886801e-10j], [8.96724049e-12 + 1.94724287e-11j, 8.52751518e-12 - 1.94724287e-11j], [3.07462056e-11 + 3.76591861e-11j, 2.89871868e-11 - 3.76591861e-11j]],
      ),
     (levitate.algorithms.RadiationForce,
         [1.83399145e-10, 4.15099186e-10, 6.22648779e-10],
-        [[2.03139282e-10, 1.63659008e-10], [4.04354167e-10, 4.25844205e-10], [6.06531251e-10, 6.38766308e-10]],
-        [[3.89064704e-10, -3.89064704e-10], [8.13263002e-10, -8.13263002e-10], [1.21989450e-09, -1.21989450e-09]]
+        [[2.03139282e-10 + 3.89064704e-10j, 1.63659008e-10 - 3.89064704e-10j], [4.04354167e-10 + 8.13263002e-10j, 4.25844205e-10 - 8.13263002e-10j], [6.06531251e-10 + 1.21989450e-09j, 6.38766308e-10 - 1.21989450e-09j]],
      ),
     (lambda arr: abs(levitate.algorithms.Pressure(arr)),
         2.10706889e+02,
-        [2.07034544e+02, 2.14379234e+02],
-        [4.15076576e+02, -4.15076576e+02],
+        [2.07034544e+02 + 4.15076576e+02j, 2.14379234e+02 - 4.15076576e+02j],
      ),
     (lambda arr: abs(levitate.algorithms.Velocity(arr)),
         [8.93991803e-05, 3.55387889e-04, 7.99622751e-04],
-        [[1.07974283e-04, 7.08240775e-05], [3.43002548e-04, 3.67773230e-04], [7.71755733e-04, 8.27489769e-04]],
-        [[0.000174546016, -0.000174546016], [0.000699933899, -0.000699933899], [0.001574851272, -0.001574851272]]
+        [[1.07974283e-04 + 0.000174546016j, 7.08240775e-05 - 0.000174546016j], [3.43002548e-04 + 0.000699933899j, 3.67773230e-04 - 0.000699933899j], [7.71755733e-04 + 0.001574851272j, 8.27489769e-04 - 0.001574851272j]],
      ),
 ])
-def test_algorithm(algorithm, value_at_pos_1, real_jacobian_at_pos_1, imag_jacobian_at_pos_1):
+def test_algorithm(algorithm, value_at_pos_1, jacobian_at_pos_1):
     algorithm = algorithm(array)
-    calc_values, calc_jacobians = algorithm.values, algorithm.jacobians
 
-    val_1 = calc_values(pressure_derivs_summed=sum_ders[..., 0])
-    val_2 = calc_values(pressure_derivs_summed=sum_ders[..., 1])
-    val_12 = calc_values(pressure_derivs_summed=sum_ders)
+    val_1 = algorithm.values(**{key: requirements[key][..., 0] for key in algorithm.values_require})
+    val_2 = algorithm.values(**{key: requirements[key][..., 1] for key in algorithm.values_require})
+    val_12 = algorithm.values(**{key: requirements[key] for key in algorithm.values_require})
     np.testing.assert_allclose(val_1, np.array(value_at_pos_1))
     np.testing.assert_allclose(val_12, np.stack([val_1, val_2], -1))
 
-    jac_1 = calc_jacobians(pressure_derivs_summed=sum_ders[..., 0], pressure_derivs_individual=ind_ders[..., 0])
-    jac_2 = calc_jacobians(pressure_derivs_summed=sum_ders[..., 1], pressure_derivs_individual=ind_ders[..., 1])
-    jac_12 = calc_jacobians(pressure_derivs_summed=sum_ders, pressure_derivs_individual=ind_ders)
-    np.testing.assert_allclose(jac_1.real, np.array(real_jacobian_at_pos_1))
-    np.testing.assert_allclose(jac_1.imag, np.array(imag_jacobian_at_pos_1))
-    np.testing.assert_allclose(jac_12, np.stack([jac_1, jac_2], -1))
+    if jacobian_at_pos_1 is not None:
+        jac_1 = algorithm.jacobians(**{key: requirements[key][..., 0] for key in algorithm.jacobians_require})
+        jac_2 = algorithm.jacobians(**{key: requirements[key][..., 1] for key in algorithm.jacobians_require})
+        jac_12 = algorithm.jacobians(**{key: requirements[key] for key in algorithm.jacobians_require})
+        np.testing.assert_allclose(jac_1, jacobian_at_pos_1)
+        np.testing.assert_allclose(jac_12, np.stack([jac_1, jac_2], -1))
