c025bd389ebcc3016ae5b2baf832a3ac70cfd761
Carl Andersson <carl.andersson@chalmers.se>
2019-06-19 11:57:00 +0200

    Adds more algorithm tests
    Some of the radiation force algorithms had no tests. The values are
    from the current implementation.

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -89,34 +89,46 @@ requirements = dict(
 )
 
 
-@pytest.mark.parametrize("algorithm, value_at_pos_1, jacobian_at_pos_1", [
-    (levitate.algorithms.Pressure,
+@pytest.mark.parametrize("algorithm, kwargs, value_at_pos_1, jacobian_at_pos_1", [
+    (levitate.algorithms.Pressure, {},
         12.068916910969428 + 8.065242302836108j,
         [-2.014671808191e+00 + 1.584976293557e+01j, +1.408358871916e+01 - 7.784520632737e+00j]
      ),
-    (levitate.algorithms.Velocity,
+    (levitate.algorithms.Velocity, {},
         [+7.327894037353e-03 + 5.975043873706e-03j, +1.570939268938e-02 + 1.042127010721e-02j, +2.356408903408e-02 + 1.563190516081e-02j],
         [[-1.407708646094e-03 + 1.076187601421e-02j, +8.735602683448e-03 - 4.786832140508e-03j], [-2.681349802084e-03 + 2.049881145565e-02j, +1.839074249147e-02 - 1.007754134844e-02j], [-4.022024703126e-03 + 3.074821718347e-02j, +2.758611373720e-02 - 1.511631202266e-02j]]
      ),
-    (levitate.algorithms.GorkovPotential,
+    (levitate.algorithms.GorkovPotential, {},
         -6.19402404e-13,
         [-6.08626619e-13 - 1.21656276e-12j, -6.30178190e-13 + 1.21656276e-12j],
      ),
-    (levitate.algorithms.GorkovGradient,
+    (levitate.algorithms.GorkovGradient, {},
         [2.30070037e-11, -1.62961537e-12, -2.44442306e-12],
         [[2.30839871e-11 + 1.79047948e-11j, 2.29300203e-11 - 1.79047948e-11j], [-1.69118632e-12 + 9.84604578e-13j, -1.56804442e-12 - 9.84604578e-13j], [-2.53677948e-12 + 1.47690687e-12j, -2.35206663e-12 - 1.47690687e-12j]],
      ),
-    (levitate.algorithms.GorkovLaplacian,
+    (levitate.algorithms.GorkovLaplacian, {},
         [-3.98121194e-10, 8.74737783e-12, 2.98666962e-11],
         [[-3.98912624e-10 + 3.33886801e-10j, -3.97329763e-10 - 3.33886801e-10j], [8.96724049e-12 + 1.94724287e-11j, 8.52751518e-12 - 1.94724287e-11j], [3.07462056e-11 + 3.76591861e-11j, 2.89871868e-11 - 3.76591861e-11j]],
      ),
-    (levitate.algorithms.RadiationForce,
+    (levitate.algorithms.RadiationForce, {},
         [1.83399145e-10, 4.15099186e-10, 6.22648779e-10],
         [[2.03139282e-10 + 3.89064704e-10j, 1.63659008e-10 - 3.89064704e-10j], [4.04354167e-10 + 8.13263002e-10j, 4.25844205e-10 - 8.13263002e-10j], [6.06531251e-10 + 1.21989450e-09j, 6.38766308e-10 - 1.21989450e-09j]],
      ),
+    (levitate.algorithms.RadiationForceStiffness, {},
+        [-5.377916683452e-09, +2.563628836166e-09, +3.173794966908e-09],
+        [[-5.519896755958e-09 - 2.548378109047e-09j, -5.235936610946e-09 + 2.548378109047e-09j], [+2.586556763000e-09 + 2.227770294366e-09j, +2.540700909331e-09 - 2.227770294367e-09j], [+3.292539171218e-09 - 7.041060162908e-11j, +3.055050762598e-09 + 7.041060162897e-11j]]
+     ),
+    (levitate.algorithms.RadiationForceCurl, {},
+        [+2.615958687199e-23, +2.391674937388e-08, -1.594449958258e-08],
+        [[+1.242838870361e-22 - 2.905471901593e-23j, -9.150685526368e-24 + 7.961613395823e-24j], [+2.391674937388e-08 + 1.748702221918e-08j, +2.391674937388e-08 - 1.748702221918e-08j], [-1.594449958258e-08 - 1.165801481279e-08j, -1.594449958258e-08 + 1.165801481279e-08j]]
+     ),
+    (levitate.algorithms.RadiationForceGradient, {},
+        [[-5.377916683452e-09, +2.991379534396e-10, +4.487069301596e-10], [-1.564536162914e-08, +2.563628836166e-09, +7.321993568902e-10], [-2.346804244372e-08, +7.321993568902e-10, +3.173794966908e-09]],
+        None
+     ),
 ])
-def test_algorithm(algorithm, value_at_pos_1, jacobian_at_pos_1):
-    algorithm = algorithm(array).algorithm
+def test_algorithm(algorithm, kwargs, value_at_pos_1, jacobian_at_pos_1):
+    algorithm = algorithm(array, **kwargs).algorithm
 
     val_1 = algorithm.values(**{key: requirements[key][..., 0] for key in algorithm.values_require})
     val_2 = algorithm.values(**{key: requirements[key][..., 1] for key in algorithm.values_require})
