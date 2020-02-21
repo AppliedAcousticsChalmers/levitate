2b1783ad367e5dd61f86b7a97cf2cf773787acca
Carl Andersson <carl.andersson@chalmers.se>
2020-02-21 16:01:10 +0100

    Updates to visualization
    Adds nicer string formating for a lot of the text in the figures.
    When the TeX rendering of text is not available, we have to resors to
    manual application of html tags.
    
    Adds an option to project the array visualizations on a given axis plane.
    Userful for static "2d"  plots.

--- levitate/visualizers.py
+++ levitate/visualizers.py
@@ -1,4 +1,5 @@
 """Visualization classes based on the plotly graphing library."""
+# DOCS: This entire module needs documentation...
 import collections.abc
 import numpy as np
 try:
@@ -8,8 +9,60 @@ except ImportError:
         return dict(data=data, layout=layout, **kwargs)
 
 
+def _string_formatter(string, format_type):
+    if callable(format_type):
+            return format_type(string)
+    format_type = format_type.lower()
+    if format_type == 'html':
+        # **...**
+        parts = string.split('**')
+        parts[1::2] = map('<b>{}</b>'.format, parts[1::2])
+        string = ''.join(parts)
+        # _{...}
+        parts = string.split('_{')
+        parts[1:] = map(lambda s: '<sub>' + s.replace('}', '</sub>', 1), parts[1:])
+        string = ''.join(parts)
+        # _
+        parts = string.split('_')
+        parts[1:] = map(lambda s: '<sub>' + s[0] + '</sub>' + s[1:], parts[1:])
+        string = ''.join(parts)
+        # ^{...}
+        parts = string.split('^{')
+        parts[1:] = map(lambda s: '<sup>' + s.replace('}', '</sup>', 1), parts[1:])
+        string = ''.join(parts)
+        # ^
+        parts = string.split('^')
+        parts[1:] = map(lambda s: '<sup>' + s[0] + '</sup>' + s[1:], parts[1:])
+        string = ''.join(parts)
+        # $...$
+        parts = string.split('$')
+        parts[1::2] = map('<i>{}</i>'.format, parts[1::2])
+        string = ''.join(parts)
+        # multiplication dots
+        string = string.replace('*', u'\u2219')
+        return string
+    if format_type == 'latex':
+        string = string.replace('*', ' \\cdot ')
+        string_parts = string.split('$')
+        string_parts[::2] = map('\\text{{{}}}'.format, string_parts[::2])
+        return '$' + ''.join(string_parts) + '$'
+    return string
+
+
+def _deepupdate(original, update):
+    if not isinstance(original, collections.abc.Mapping):
+        return update
+    for key, value in update.items():
+        if isinstance(value, collections.abc.Mapping):
+            original[key] = _deepupdate(original.get(key, {}), value)
+        else:
+            original[key] = value
+    return original
+
+
 class Visualizer(collections.abc.MutableSequence):
     template = 'plotly_white'
+    string_format = 'html'
 
     def __init__(self, array, *traces, display_scale='mm'):
         self.array = array
@@ -36,7 +89,7 @@ class Visualizer(collections.abc.MutableSequence):
 
     @property
     def layout(self):
-        return dict(template=self.template)
+        return dict(template=self.template, font=dict(family='CMU Serif, Latin Modern, Times New Roman'))
 
     @property
     def display_scale(self):
@@ -151,7 +204,52 @@ class ArrayVisualizer(Visualizer):
 
     @property
     def layout(self):
-        return dict(super().layout, scene=dict(aspectmode='data'))
+        return _deepupdate(
+            super().layout, dict(scene=dict(
+                aspectmode='data',
+                xaxis=dict(title=_string_formatter('$x$ in ' + self.display_scale, self.string_format)),
+                yaxis=dict(title=_string_formatter('$y$ in ' + self.display_scale, self.string_format)),
+                zaxis=dict(title=_string_formatter('$z$ in ' + self.display_scale, self.string_format))
+            )))
+
+    def projection_layout(self, plane='xz', scale=None, layout=None):
+        scale = 1 if scale is None else scale
+        new = {'scene': {'aspectmode': 'manual', 'camera': {'projection': {'type': 'orthographic'}}}}
+        if plane == 'xy':
+            new['scene']['zaxis'] = {'visible': False}
+            new['scene']['camera']['eye'] = dict(x=0, y=0, z=1)
+            new['scene']['camera']['up'] = dict(x=0, y=1, z=0)
+            new['scene']['aspectratio'] = dict(x=scale, y=scale, z=1)
+        elif plane == 'yx':
+            new['scene']['zaxis'] = {'visible': False}
+            new['scene']['camera']['eye'] = dict(x=0, y=0, z=-1)
+            new['scene']['camera']['up'] = dict(x=1, y=0, z=0)
+            new['scene']['aspectratio'] = dict(x=scale, y=scale, z=1)
+        elif plane == 'xz':
+            new['scene']['yaxis'] = {'visible': False}
+            new['scene']['camera']['eye'] = dict(x=0, y=-1, z=0)
+            new['scene']['camera']['up'] = dict(x=0, y=0, z=1)
+            new['scene']['aspectratio'] = dict(x=scale, y=1, z=scale)
+        elif plane == 'zx':
+            new['scene']['yaxis'] = {'visible': False}
+            new['scene']['camera']['eye'] = dict(x=0, y=1, z=0)
+            new['scene']['camera']['up'] = dict(x=1, y=0, z=0)
+            new['scene']['aspectratio'] = dict(x=scale, y=1, z=scale)
+        elif plane == 'yz':
+            new['scene']['xaxis'] = {'visible': False}
+            new['scene']['camera']['eye'] = dict(x=1, y=0, z=0)
+            new['scene']['camera']['up'] = dict(x=0, y=0, z=1)
+            new['scene']['aspectratio'] = dict(x=1, y=scale, z=scale)
+        elif plane == 'zy':
+            new['scene']['xaxis'] = {'visible': False}
+            new['scene']['camera']['eye'] = dict(x=-1, y=0, z=0)
+            new['scene']['camera']['up'] = dict(x=0, y=1, z=0)
+            new['scene']['aspectratio'] = dict(x=1, y=scale, z=scale)
+        else:
+            raise ValueError('Unknown projection plane `{}`'.format(plane))
+        # TODO: Handle plotly Layout objects, plotly Figures, and dicts corresponding to figures.
+        # The difficulty is that Layout objects and Figures doesn't have a setdefault method.
+        return _deepupdate({} if layout is None else layout, new)
 
     def __call__(self, *complex_transducer_amplitudes, **kwargs):
         traces = []
@@ -163,9 +261,10 @@ class ArrayVisualizer(Visualizer):
             button_labels = ['{}']
         else:
             if 'labels' in kwargs:
-                button_labels = [kwargs['labels'][idx] + ' | {}' for idx in range(len(complex_transducer_amplitudes))]
+                labels = kwargs.pop('labels')
+                button_labels = [labels[idx] + ' | {}' for idx in range(len(complex_transducer_amplitudes))]
             else:
-                label = kwargs.get('label', 'State')
+                label = kwargs.pop('label', 'State')
                 button_labels = [label + ' {} | {{}}'.format(idx) for idx in range(len(complex_transducer_amplitudes))]
 
         trace_idx = 0
@@ -207,7 +306,9 @@ class ArrayVisualizer(Visualizer):
                 if field_idx > 0:
                     traces[trace_idx]['visible'] = False
             updatemenus.append(dict(active=0, buttons=buttons, type='buttons', direction='down', x=1.02, xanchor='left'))
-        layout = dict(self.layout, updatemenus=updatemenus)
+        layout = _deepupdate(self.layout, {'updatemenus': updatemenus})
+        if 'projection' in kwargs:
+            self.projection_layout(layout=layout, plane=kwargs.pop('projection'), scale=kwargs.pop('scale', None))
         return Figure(data=traces, layout=layout)
 
 
@@ -224,6 +325,20 @@ class Trace:
             return self.visualizer._display_scale
         except AttributeError:
             return 1
+
+    @property
+    def string_format(self):
+        try:
+            return self._stirng_format
+        except AttributeError:
+            try:
+                return self.visualizer.string_format
+            except AttributeError:
+                return Visualizer.stirng_format
+
+    @string_format.setter
+    def string_format(self, val):
+        self._string_format = val
 
 
 class TrapPath(Trace):
@@ -373,7 +488,7 @@ class TransducerTrace(MeshTrace):
             i=indices[0], j=indices[1], k=indices[2],
             intensity=intensity,
             colorscale=self.colorscale, showscale=self.showscale,
-            colorbar={'title': {'text': self.label, 'side': 'right'}, 'x': -0.02, 'xanchor': 'left'},
+            colorbar={'title': {'text': _string_formatter(self.label, self.string_format), 'side': 'right'}, 'x': -0.02, 'xanchor': 'left'},
             cmin=self.cmin, cmax=self.cmax,
         )
 
@@ -592,7 +707,7 @@ class ScalarFieldSlice(FieldTrace):
         return dict(
             type='mesh3d', intensity=super().__call__(complex_transducer_amplitudes),
             cmin=self.cmin, cmax=self.cmax, colorscale=self.colorscale,
-            colorbar={'title': {'text': self.label, 'side': 'right'}, 'x': 1.02, 'xanchor': 'right'},
+            colorbar={'title': {'text': _string_formatter(self.label, self.string_format), 'side': 'right'}, 'x': 1.02, 'xanchor': 'right'},
             x=np.squeeze(self.mesh[0]) / self.display_scale,
             y=np.squeeze(self.mesh[1]) / self.display_scale,
             z=np.squeeze(self.mesh[2]) / self.display_scale,
@@ -668,7 +783,7 @@ class VectorFieldCones(FieldTrace):
             x=vertex_coordinates[0], y=vertex_coordinates[1], z=vertex_coordinates[2],
             i=vertex_indices[0], j=vertex_indices[1], k=vertex_indices[2],
             intensity=vertex_intensities, opacity=self.opacity, colorscale=self.colorscale,
-            colorbar={'title': {'text': self.label, 'side': 'right'}, 'x': 1.02, 'xanchor': 'right'},
+            colorbar={'title': {'text': _string_formatter(self.label, self.string_format), 'side': 'right'}, 'x': 1.02, 'xanchor': 'right'},
         )
 
     def _generate_vertices(self, field_data):
@@ -745,7 +860,7 @@ class ForceCones(VectorFieldCones):
     def label(self):
         try:
             if self.scale_to_gravity:
-                return "$|F|/mg$"
+                return "|$F$|$/mg$"
         except AttributeError:
             pass
         return 'Force magnitude in N'
@@ -794,7 +909,7 @@ class ForceDiagram(Visualizer):
             for idx, data in enumerate(complex_transducer_amplitudes):
                 this_state_traces = field(
                     data, line=kwargs['line'][idx] if 'line' in kwargs else dict(color=colors[idx % len(colors)]),
-                    name=kwargs['name'][idx] if 'name' in kwargs else 'Data {}'.format(idx),
+                    name=_string_formatter(kwargs['name'][idx] if 'name' in kwargs else 'Data {}'.format(idx), self.string_format),
                 )
                 all_traces.extend(this_state_traces)
         elif len(self) > 1:
@@ -802,7 +917,7 @@ class ForceDiagram(Visualizer):
             for idx, field in enumerate(self):
                 this_field_traces = field(
                     data, line=kwargs['line'][idx] if 'line' in kwargs else dict(color=colors[idx % len(colors)]),
-                    name=kwargs['name'][idx] if 'name' in kwargs else field.name if field.name is not '' else 'Force {}'.format(idx),
+                    name=_string_formatter(kwargs['name'][idx] if 'name' in kwargs else field.name if field.name is not '' else 'Force {}'.format(idx), self.string_format),
                 )
                 all_traces.extend(this_field_traces)
         else:
@@ -819,17 +934,17 @@ class ForceDiagram(Visualizer):
         total_pieces = layout_gap_ratio * 3 + 2
         width = layout_gap_ratio / total_pieces
         gap = 1 / total_pieces
-        length_unit = r'\text{{ in {}}}'.format(self.display_scale)
-        force_unit = '/mg' if self.scale_to_gravity else r'\text{ in N}'
-        return dict(
-            super().layout,
-            xaxis=dict(title='$x' + length_unit + '$', domain=[0, width], anchor='y3'),
-            xaxis2=dict(title='$y' + length_unit + '$', domain=[width + gap, 2 * width + gap], anchor='y3'),
-            xaxis3=dict(title='$z' + length_unit + '$', domain=[2 * width + 2 * gap, 1], anchor='y3'),
-            yaxis=dict(title='$F_x' + force_unit + '$', domain=[2 * width + 2 * gap, 1]),
-            yaxis2=dict(title='$F_y' + force_unit + '$', domain=[width + gap, 2 * width + gap]),
-            yaxis3=dict(title='$F_z' + force_unit + '$', domain=[0, width]),
-        )
+        length_unit = ' in {}'.format(self.display_scale)
+        force_unit = '$/mg$' if self.scale_to_gravity else ' in N'
+        return _deepupdate(
+            super().layout, dict(
+                xaxis=dict(title=_string_formatter('$x$' + length_unit, self.string_format), domain=[0, width], anchor='y3'),
+                xaxis2=dict(title=_string_formatter('$y$' + length_unit, self.string_format), domain=[width + gap, 2 * width + gap], anchor='y3'),
+                xaxis3=dict(title=_string_formatter('$z$' + length_unit, self.string_format), domain=[2 * width + 2 * gap, 1], anchor='y3'),
+                yaxis=dict(title=_string_formatter('$F_x$' + force_unit, self.string_format), domain=[2 * width + 2 * gap, 1]),
+                yaxis2=dict(title=_string_formatter('$F_y$' + force_unit, self.string_format), domain=[width + gap, 2 * width + gap]),
+                yaxis3=dict(title=_string_formatter('$F_z$' + force_unit, self.string_format), domain=[0, width]),
+            ))
 
     class ForceTrace(FieldTrace):
         _trace_objects = 0
