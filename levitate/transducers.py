Fixes sign error in source positions
A long standing error in the implementation caused the array to be
virtually mirrored in the x-axis. This will break all consistency tests,
which will have to be recreated.

levitate/transducers.py
@@ -1246,7 +1246,7 @@ class CylinderModes(TransducerModel):
 
         n_max = self._maximum_bessel_order(k_max)
         for n in range(n_max + 1):
-            angle = n * (theta_rec + theta_src)
+            angle = n * (theta_rec - theta_src)
             cos_angle = np.cos(angle)
             sin_angle = np.sin(angle)
             for s, radial_wavenumber in enumerate(self._bessel_deriv_zeros(n, k_max), 1):